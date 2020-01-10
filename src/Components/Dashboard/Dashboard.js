8f28fa8104585bf0d48787fa457f809e1071c84f
Matt Bodily <mattcbodily@gmail.com>
2020-01-10 16:16:15 -0700

    able to toggle view in the group component between creating groups and viewing groups

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from '../Header/Header';
 import Groups from '../Groups/Groups';
 import Message from '../Message/Message';
 
 export default () => {
+    const [member, setMember] = useState({});
+    const [groups, setGroups] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/member')
+        .then(res => setMember(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
             <Header />
--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Groups.css';
 
 export default () => {
+    const [groups, setGroups] = useState([]);
+    const [createGroupView, setCreateGroupView] = useState(false);
+    const [groupName, setGroupName] = useState('');
+    const [groupDescription, setGroupDescription] = useState('');
+    const [privateGroup, setPrivateGroup] = useState(false);
+
     return (
         <div className='groups'>
-            Groups Component
+            {!createGroupView
+            ? (<>
+                <button onClick={() => setCreateGroupView(true)}>Create Group</button>
+                {/* groups will go here */}
+               </>)
+            : (<>
+                <input
+                    value={groupName}
+                    maxLength='20'
+                    onChange={(e) => setGroupName(e.target.value)}/>
+                <textarea 
+                    value={groupDescription}
+                    maxLength='500'
+                    onChange={(e) => setGroupDescription(e.target.value)}/>
+                <input 
+                    type='checkbox'
+                    value={privateGroup}
+                    onChange={(e) => setPrivateGroup(e.target.value)}/>
+                <button>Create</button>
+                <button onClick={() => setCreateGroupView(false)}>Cancel</button>
+               </>)}
         </div>
     )
 };
