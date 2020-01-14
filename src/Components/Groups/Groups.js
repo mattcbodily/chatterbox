0954dc77efe570c3ef87ad79cfd66191664fd73d
Matt Bodily <mattcbodily@gmail.com>
2020-01-14 09:51:28 -0700

    converting Groups to class component to more easily work with sockets

--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -1,29 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
+import io from 'socket.io-client';
 import axios from 'axios';
 import Message from '../Message/Message';
 import './Groups.css';
 
-export default (props) => {
-    const [groups, setGroups] = useState([]);
-    const [selectedGroup, setSelectedGroup] = useState({});
-    const [createGroupView, setCreateGroupView] = useState(false);
-    const [groupName, setGroupName] = useState('');
-    const [groupDescription, setGroupDescription] = useState('');
-    const [privateGroup, setPrivateGroup] = useState(false);
-
-    useEffect(() => {
-        if(props.member.member_id){
-            getGroups(props.member.member_id)
+class Groups extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            groups: [],
+            selectedGroup: {},
+            createGroupView: false,
+            groupName: '',
+            groupDescription: '',
+            privateGroup: false
         }
-    }, [props.member.member_id])
+    }
 
-    const getGroups = (id) => {
+    componentDidMount(){
+        this.getGroups(this.props.member.member_id);
+    }
+
+    getGroups = (id) => {
         axios.get(`/api/groups/${id}`)
         .then(res => setGroups(res.data))
         .catch(err => console.log(err))
     }
 
-    const createGroup = () => {
+    createGroup = () => {
         let newGroup = {
             id: props.member.member_id,
             groupName,
@@ -33,41 +37,54 @@ export default (props) => {
 
         axios.post('/api/group', newGroup)
         .then(res => {
-            props.getGroupsFn(props.member.member_id);
-            setCreateGroupView(false);
+            this.props.getGroupsFn(this.props.member.member_id);
+            // setCreateGroupView(false);
         })
         .catch(err => console.log(err));
     }
 
-    const mappedGroups = groups.map((group, i) => {
+    handleInputs = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render(){
+        mappedGroups = this.state.groups.map((group, i) => {
+            return (
+                // need to set selected group here
+                <p key={i}>{group.group_name}</p>
+            )
+        })
         return (
-            <p key={i} onClick={() => setSelectedGroup(group.group_id)}>{group.group_name}</p>
+            <div className='groups'>
+                {!createGroupView
+                ? (<>
+                    <button onClick={() => setCreateGroupView(true)}>Create Group</button>
+                    {mappedGroups}
+                </>)
+                : (<>
+                    <input
+                        value={this.state.groupName}
+                        name='groupName'
+                        maxLength='20'
+                        onChange={(e) => this.handleInputs(e)}/>
+                    <textarea 
+                        value={this.state.groupDescription}
+                        name='groupDescription'
+                        maxLength='500'
+                        onChange={(e) => this.handleInputs(e)}/>
+                    <input 
+                        type='checkbox'
+                        value={this.state.privateGroup}
+                        name='privateGroup'
+                        onChange={(e) => this.handleInputs(e)}/>
+                    <button onClick={this.createGroup}>Create</button>
+                    {/* need to toggle createGroupView here */}
+                    <button>Cancel</button>
+                </>)}
+                <Message member={this.props.member} selectedGroup={this.state.selectedGroup}/>
+            </div>
         )
-    })
-    return (
-        <div className='groups'>
-            {!createGroupView
-            ? (<>
-                <button onClick={() => setCreateGroupView(true)}>Create Group</button>
-                {mappedGroups}
-               </>)
-            : (<>
-                <input
-                    value={groupName}
-                    maxLength='20'
-                    onChange={(e) => setGroupName(e.target.value)}/>
-                <textarea 
-                    value={groupDescription}
-                    maxLength='500'
-                    onChange={(e) => setGroupDescription(e.target.value)}/>
-                <input 
-                    type='checkbox'
-                    value={privateGroup}
-                    onChange={(e) => setPrivateGroup(e.target.value)}/>
-                <button onClick={createGroup}>Create</button>
-                <button onClick={() => setCreateGroupView(false)}>Cancel</button>
-               </>)}
-            <Message member={props.member} selectedGroup={selectedGroup}/>
-        </div>
-    )
+    }
 };
