b6f25ea92e1c445ac88fc68fb63844335a5a0e8b
Matt Bodily <mattcbodily@gmail.com>
2020-01-15 16:50:12 -0700

    added dropdown menu to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import axios from 'axios';
 import './Header.scss';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
+library.add(faAngleDown)
 
 const Header = (props) => {
+    const [showDropDown, setShowDropDown] = useState(false);
+
     const logout = () => {
         axios.post('/api/logout').then(res => {
             props.history.push('/')
@@ -14,8 +20,12 @@ const Header = (props) => {
     return (
         <header className='header'>
                 <img src={props.member.avatar} alt='member avatar' className='avatar-image'/>
-                <span>{props.member.username}</span>
-                <button onClick={logout}>Logout</button>
+                <span onClick={() => setShowDropDown(!showDropDown)}>{props.member.username}<FontAwesomeIcon icon='angle-down' className='menu-icon'/></span>
+                {showDropDown
+                ? (<section className='dropdown'>
+                    <p onClick={logout}>Logout</p>
+                   </section>)
+                : null}
         </header>
     )
 }
